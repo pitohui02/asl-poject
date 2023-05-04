27c42b4f63fc84f374fa125be952ce5fb2e4f400
prLorence <paulorencereyes@gmail.com>
2023-05-04 19:38:13 +0800

    feeat: container pattern for fetching data from server

---
+++ functions/ResidentContainer.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Presenter from '../components/table';
+
+export type Resident = {
+  id: number;
+  profilePhotoUrl: string;
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  maritalStatus: string;
+  homeAddress: string;
+  gender: string;
+  postalCode: string;
+  guardian: string;
+  age: number;
+  mother: string;
+  father: string;
+  telephoneNumber: string;
+  mobileNumber: string;
+  birthDate: string;
+  createdAt: string;
+  updatedAt: string;
+  isArchived: boolean;
+}
+
+export default function ResidentContainer() {
+  const [residents, setResidents] = useState<Resident[]>([]);
+
+  // feed residents to data prop of table
+
+  useEffect(() => {
+    axios.get(`${process.env.apiUrl}/resident`, {
+      headers: {
+        "Authorization": localStorage.getItem('jwt')
+      }
+    })
+      .then(res => setResidents(res.data)).catch(err => console.log(err));
+  }, [])
+
+  return (
+    <>
+      <Presenter tableData={residents} />
+    </>
+  )
+
+}
+
+
+
