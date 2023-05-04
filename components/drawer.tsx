3a6e7209188626e8e88fd75b170ee8f3a6456805
prLorence <paulorencereyes@gmail.com>
2023-05-05 00:32:01 +0800

    feat: added modal for adding resident, printing certificates

--- components/drawer.tsx
+++ components/drawer.tsx
@@ -1,87 +1,52 @@
-import React from "react"
-import { Drawer, Box, Typography, Paper, Button } from "@mui/material"
+import React from 'react';
+import { Drawer, Box, Typography, Paper, Button } from '@mui/material';
 
-import styles from '../src/styles/drawer.module.css'
+import styles from '../src/styles/drawer.module.css';
 
-import { withRouter } from "next/router"
+import { withRouter } from 'next/router';
+import EditModal from './modals/EditModal';
+import CreateModal from './modals/CreateModal';
+import PrintModal from './modals/PrintModal';
 
 class DrawerComponent extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+  }
 
-    constructor(props: any) {
-        super(props)
+  handleLogOut = () => {
+    localStorage.removeItem('jwt');
+    this.props.router.push('/login');
+  };
 
-    }
+  render() {
+    return (
+      <Box>
+        <Drawer anchor="left" variant="permanent">
+          <Box className={styles.drawerbox}>
+            <Typography variant="h5" className={styles.drawerTitle}>
+              BARANGAY 15
+            </Typography>
 
-    handleLogOut = () => {
+            <Box className={styles.groupButton}>
+              <CreateModal />
 
-        this.props.router.push('/login')
+              <PrintModal />
 
-    }
-
-    handleRegister = () => {
-
-        this.props.router.push('/registration')
-
-    }
-
-    render() {
-
-        return (
-
-            <Box >
-                    
-                <Drawer anchor = "left" variant = "permanent">
-                        
-                    <Box className = {styles.drawerbox}>
-
-                        <Typography variant = "h5" className = {styles.drawerTitle}>BARANGAY 15</Typography>
-                            
-
-                        <Box className = {styles.groupButton}>
-
-                            <Button
-                            variant= "contained" 
-                            type = "submit"
-                            className = {styles.registerButton}
-                            onClick={this.handleRegister}
-                            >
-                                REGISTER A RESIDENT
-
-                            </Button>
-
-
-                            <Button
-                            variant= "contained" 
-                            type = "submit"
-                            className = {styles.printButton}
-                            >
-                                PRINT CERTIFICATE
-
-                            </Button>
-
-                            <Button 
-                            variant= "contained" 
-                            type = "submit"
-                            className = {styles.logoutButton}
-                            onClick={this.handleLogOut}> LOG OUT
-                            </Button>
-
-                        </Box>
-    
-                    </Box>  
-                    
-                </Drawer>
-
-      
-                
+              <Button
+                variant="contained"
+                type="submit"
+                className={styles.logoutButton}
+                onClick={this.handleLogOut}
+              >
+                {' '}
+                LOG OUT
+              </Button>
             </Box>
-
-
-
-
-
-        )
-    }
+          </Box>
+        </Drawer>
+      </Box>
+    );
+  }
 }
 
-export default withRouter(DrawerComponent)
+export default withRouter(DrawerComponent);
