f5e256cbfa5ba70d90669d73a9dc5622d6ba4a93
prLorence <paulorencereyes@gmail.com>
2023-05-07 11:20:42 +0800

    chore: formatting

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,21 +1,29 @@
+import React from 'react';
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
+import Image from 'next/image';
 
-import React from "react";
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
-import Image from "next/image";
+import Appbar from '../../components/AppbarHome';
+import Content from '../../components/HomepageContent';
 
-import Appbar from "../../components/AppbarHome";
-import Content from '../../components/HomepageContent'
+import { withRouter } from 'next/router';
 
-import { withRouter } from "next/router";
+class Homepage extends React.Component<any, any> {
+  constructor(props: any) {
+    super(props);
+  }
 
-class Homepage extends React.Component<any, any>{
-    render() {
-        return (
-            <>
-                <Appbar />
-                <Content />
-            </>
-        )
-    }
+  handleLogin = () => {
+    this.props.router.push('/login');
+  };
+
+  render() {
+    return (
+      <>
+        <Appbar />
+        <Content />
+      </>
+    );
+  }
+}
 
 export default withRouter(Homepage);
